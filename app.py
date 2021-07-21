e56aed7082b0a45c5a704c4c974298bf1acb3387
raghu <dasigiraghu96@gmail.com>
2021-07-21 12:15:15 +0530

    third commit

--- app.py
+++ app.py
@@ -4,15 +4,9 @@ Created on Sat Jun  8 17:06:01 2019
 @author: prithvi
 """
 import flask
-from flask import Flask, request, jsonify, render_template
-import jinja2
-import numpy as np
-import pandas as pd
-import pickle
-
 from flask import Flask, request,jsonify,render_template
 import pickle
-from flask_cors import CORS,cross_origin
+#from flask_cors import CORS,cross_origin
 import pandas as pd
 import numpy as np
 
@@ -21,13 +15,13 @@ filename = 'Model_GYM.pkl'
 model = pickle.load(open(filename, 'rb'))
 
 @app.route('/')
-@cross_origin
+#@cross_origin
 def man():
     return render_template('home.html')
 
 
 @app.route('/predict', methods=['POST'])
-@cross_origin
+#@cross_origin
 def home():
     data1 = request.form['a']
     data2 = request.form['b']
